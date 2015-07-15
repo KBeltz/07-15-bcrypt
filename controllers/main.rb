9b2d5ede5660c23a35ebf9b569b130bbbf2b86fe
Kristin Beltz <beltz08@me.com>
2015-07-15 15:43:30 -0500

    Sign up and login pages.

--- controllers/main.rb
+++ controllers/main.rb
@@ -1,7 +1,15 @@
 # ---------------------------------------------------------------------
 # Main
 # ---------------------------------------------------------------------
-get "/home" do
+get "/" do
   @page_title = "Home"
   erb :"/main/home"
+end
+
+get "/new" do
+  erb :"/main/new"
+end
+
+get "/login" do
+  erb :"/main/login"
 end
